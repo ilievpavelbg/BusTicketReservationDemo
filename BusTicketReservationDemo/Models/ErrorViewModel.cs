76d7447f27662b1921e66a9a9d5aa50ad1190d6e
Pavel-Iliev-1 <pvl_iliev@abv.bg>
2022-09-30 16:05:26 +0300

    Add project files.

---
+++ BusTicketReservationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusTicketReservationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
