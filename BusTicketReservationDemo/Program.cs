cf5f9231dc039bc2328924f0d449935b4b81866d
Pavel-Iliev-1 <pvl_iliev@abv.bg>
2022-09-30 17:20:39 +0300

    Remove Migrations and DBContext to CORE and DB Entities was created in CORE.Data

--- BusTicketReservationDemo/Program.cs
+++ BusTicketReservationDemo/Program.cs
@@ -1,4 +1,4 @@
-using BusTicketReservationDemo.Data;
+using BusTicketReservationDemoCORE.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
--- BusTicketReservationDemoCORE/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ BusTicketReservationDemoCORE/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Migrations;
 using System;
 
-namespace BusTicketReservationDemo.Data.Migrations
+namespace BusTicketReservationDemoCORE.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
